b0b06eb88446a694795c2b651b606a675b82ae4f
Roman <RomaBalakirev@gmail.com>
2016-07-31 15:14:37 +0300

    create rest

--- src/main/java/ru/remindme/server/controller/ReminderController.java
+++ src/main/java/ru/remindme/server/controller/ReminderController.java
@@ -1,19 +1,31 @@
 package ru.remindme.server.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import ru.remindme.server.entity.Remind;
 
-@Controller
+import java.util.Date;
+
+@RestController
 @RequestMapping("/reminder")
 public class ReminderController
 {
     @RequestMapping(value = "/get", method = RequestMethod.GET)
     @ResponseBody
-    public String getReminder(ModelMap map)
+    public Remind getReminder()
     {
-        return "My Reminder";
+        return createMockRemind();
+    }
+
+    private Remind createMockRemind()
+    {
+        Remind remind = new Remind();
+        remind.setId(1L);
+        remind.setRemindDate(new Date());
+        remind.setTitle("Mock Remind");
+        return remind;
     }
 }
